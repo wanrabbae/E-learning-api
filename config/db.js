4fa47723e7563e168381e08748ae6270149218df
wanrabbae <wancode.msg@gmail.com>
2022-11-23 12:29:17 +0700

    make it better

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 // import sequelize
+require("dotenv").config();
 const Sequelize = require("sequelize");
 
 // create connection
-const db = new Sequelize("e_learning", "root", "", {
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PW, {
   host: "localhost",
   dialect: "mysql",
 });
